5aef605f279b5fb72a8e48edb1fdf48359da9d4f
Никита Трощенко <nitrix38r@gmail.com>
2022-03-22 15:42:40 +0800

    commit

--- app/src/main/java/com/example/recyclerviewcolorsk/ColorViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewcolorsk/ColorViewHolder.kt
@@ -2,14 +2,19 @@ package com.example.recyclerviewcolorsk
 
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ColorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     // получаем ссылку на текстовое поле в каждом элементе списка
     val tv = itemView.findViewById<TextView>(R.id.color)
-
-    // TODO: добавить обработку нажатия на элемент списка (вывести Toast с кодом цвета)
     // совет: использовать блок init { }
+
+    init{
+        tv.setOnClickListener {
+            Toast.makeText(tv.context, tv.text, Toast.LENGTH_SHORT).show();
+        }
+    }
 
     fun bindTo(color: Int) {
         tv.setBackgroundColor(color)
--- app/src/main/java/com/example/recyclerviewcolorsk/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewcolorsk/MainActivity.kt
@@ -4,25 +4,30 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val planetsList = arrayListOf<String>("Mars", "Venus", "Earth")
-    // TODO: реализовать генерацию цветов определённой палитры
-    val colorsList = mutableListOf(Color.YELLOW, Color.RED, Color.GREEN, Color.MAGENTA)
+    val colorsList = generateColors()
+
+    private fun generateColors(): MutableList<Int>{
+        var colorsList = mutableListOf<Int>()
+        for (i in 1..1000){
+            colorsList.add(getRandomColor())
+        }
+        return colorsList
+    }
+
+    private fun getRandomColor(): Int {
+        val rnd = Random()
+        return Color.rgb(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // пример использования ListView
-        val lv = findViewById<ListView>(R.id.list)
-        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, planetsList)
-        lv.adapter = adapter
 
         // пример использования RecyclerView с собственным адаптером
         val rv = findViewById<RecyclerView>(R.id.rview)
